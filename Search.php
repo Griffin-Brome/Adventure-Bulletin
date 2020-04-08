a6aae63bbb13b6286aa360e7c94de2cfe3920527
Griffin Brome <griffinbrome@gmail.com>
2020-04-07 18:56:01 -0700

    start on search function

---
+++ Search.php
@@ -0,0 +1,17 @@
+<?php 
+    include 'DBConnection.php';
+    include 'Validate.php';
+
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        $search = Validate($_GET['search']);
+        try {
+            $pdo = openConnection();
+            $sql = "SELECT post_title, post_body, uname, board_title FROM post WHERE post_title LIKE %:search%";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindValue(':search', $search);
+            closeConnection($pdo);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        } 
+    }
+?>
